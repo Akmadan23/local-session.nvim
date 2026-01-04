f68079c240650987577e1d4c313f0853683b9d65
Akmadan23 <azadahmadi@mailo.com>
2026-01-04 13:27:27 +0100

    Code dump

---
+++ lua/local-session/init.lua
@@ -0,0 +1,91 @@
+local win = require("local-session.win")
+local fmt = string.format
+local api = vim.api
+
+local config = {
+    filename = ".session.lua"
+}
+
+local notify = function(msg, level)
+    vim.notify(msg, vim.log.levels[level or "WARN"], { title = "LocalSession" })
+end
+
+local M = {}
+
+M.setup = function()
+    api.nvim_create_autocmd("VimEnter", {
+        group = api.nvim_create_augroup("LocalSession", {}),
+        once = true,
+
+        callback = function(args)
+            -- try to load session only if nvim is launched with no file arguments
+            if args.file == "" then
+                vim.opt.shm:append("I") -- don't display nvim intro message
+                vim.schedule(M.load)
+            end
+        end
+    })
+end
+
+M.load = function()
+    if vim.fn.filereadable(config.filename) == 0 then
+        -- quit immediately if no session file is found
+        return
+    end
+
+    -- try to run session file
+    local ok, res = pcall(dofile, config.filename)
+
+    if not ok then
+        notify("Syntax error: " .. res, "ERROR")
+        return
+    end
+
+    if type(res) ~= "table" then
+        notify(fmt("Error: '%s' does not return a table.", config.filename))
+        return
+    end
+
+    if type(res.config) == "function" then
+        res.config()
+    elseif type(res.config) == "string" then
+        vim.cmd(res.config)
+    end
+
+    if type(res[1]) == "string" then
+        vim.cmd.edit(res[1])
+        win.setup(res)
+    elseif type(res.tabs) == "table" then
+        local first = true
+
+        local tabnew = function(file)
+            if first then
+                vim.cmd.edit(file)
+                first = false
+            else
+                vim.cmd.tabnew(file)
+            end
+        end
+
+        for _, tab in ipairs(res.tabs) do
+            if type(tab) == "string" then
+                local glob_tab = vim.fn.glob(tab)
+
+                if glob_tab == tab then
+                    tabnew(tab)
+                else
+                    for _, file in ipairs(vim.split(glob_tab, "\n")) do
+                        tabnew(file)
+                    end
+                end
+            elseif type(tab) == "table" then
+                tabnew(tab[1])
+                win.setup(tab)
+            end
+        end
+    end
+
+    api.nvim_set_current_win(win.focus_id)
+end
+
+return M
---
+++ lua/local-session/win.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+-- ID of focused window (first by default)
+M.focus_id = 1000
+
+M.setup = function(win)
+    if type(win.opts) == "table" then
+        for key, value in pairs(win.opts) do
+            vim.o[key] = value
+        end
+    end
+
+    if type(win.callback) == "string" then
+        vim.cmd(win.callback)
+    elseif type(win.callback) == "function" then
+        win.callback()
+    end
+
+    if win.focus == true then
+        M.focus_id = vim.api.nvim_get_current_win()
+    end
+
+    -- process 'split' and 'vsplit' fields
+    for _, s in ipairs { "split", "vsplit" } do
+        local split_val = win[s]
+        if type(split_val) == "string" then
+            vim.cmd[s](split_val)
+            break
+        elseif type(split_val) == "table" and type(split_val[1]) == "string" then
+            vim.cmd[s](split_val[1])
+            M.setup(split_val)
+            break
+        end
+    end
+end
+
+return M
